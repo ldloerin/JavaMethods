0cdd090049683cf12455845443bbb51aa82c0c8c
David Loerinci <david.loerinci@gmail.com>
2021-10-05 10:57:18 +0200

    added java project

---
+++ JavaProject/src/CodeOne.java
@@ -0,0 +1,9 @@
+public class CodeOne {
+    public void outputOne(){
+        System.out.println("This is CodeOne outputOne");
+    }
+
+    public void outputTwo(){
+        System.out.println("This is CodeOne outputTwo");
+    }
+}
